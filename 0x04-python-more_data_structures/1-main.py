48311d9c2637d86a977d6898516d4756ee0dc782
SethAboagye <46626532+SethAboagye@users.noreply.github.com>
2023-04-27 08:37:57 +0000

    Update 1-main.py

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{0}".format(num))
